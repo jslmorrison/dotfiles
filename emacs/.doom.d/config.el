fc342408e242405d31ee0a73683da0e463a40349
John Morrison <jslmorrison@gmail.com>
2021-12-17 13:51:18 +0000

    Set flycheck vars

--- emacs/.doom.d/config.el
+++ emacs/.doom.d/config.el
@@ -58,3 +58,6 @@
 ;;
 ;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
 ;; they are implemented.
+
+(setq flycheck-php-executable "phpcs"
+      flycheck-phpcs-standard "PSR12")
